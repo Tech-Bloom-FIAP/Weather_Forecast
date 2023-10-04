7a08993239b696c4eb398db3296820bdb2eca371
Vitor Hugo <vitorh565.vh@gmail.com>
2023-10-04 17:37:28 -0300

    Login e Cadastro

--- application.py
+++ application.py
@@ -3,7 +3,9 @@
 2- Como se portar em uma enhchente  
 3- Psicologia (TeleMedicina)
 '''
+
 import json
+
 #Exceções personalizadas
 class TelefoneError(Exception):
      pass
@@ -21,55 +23,81 @@ def formata_dados(nascimento: str = "" , cpf: str = "" , telefone: str = "" ):
 def login():
     while True:
         try:    
-            cpf = input("Digite seu CPF: ")
-            if cpf < 11 or cpf > 11:
+            cpf = formata_dados(cpf = input("Digite seu CPF: "))[1]
+            if len(cpf) < 14 or len(cpf) > 14 :
                 raise CpfError
-            senha = input("Criar uma senha: ")
+            senha = input("Digite sua uma senha: ")
             return cpf, senha
         except CpfError:
             print("O cpf informado não é valido. \nExemplo: 12345678910 \n")
     
 def cadastro():
-    with open('./arquiva.txt', 'r') as arquivo:
+    with open('./dados.txt', 'r') as arquivo:
         dados = json.load(arquivo)
     while True:
         try:    
             nome = input("Digite seu nome completo: ")
-            dados["Nome"].append(nome)
+            dados["nome"].append(nome)
+
             data = formata_dados(nascimento=input("Digite sua data de nascimento: "))[0]
+            dados["data"].append(data)
+
             cpf = formata_dados(cpf = input("Digite seu CPF: "))[1]
             if len(cpf) < 14 or len(cpf) > 14 :
                 raise CpfError
+            dados["cpf"].append(cpf)
+
             email = input("Digite seu E-mail: ")
+            dados["email"].append(email)
+
             telefone = formata_dados(telefone = input("Digite seu telefone: "))[2]
             if len(telefone) < 15 or len(telefone) > 15:
                 raise TelefoneError
+            dados["telefone"].append(nome)
+
             senha = input("Criar uma senha: ")
-            with open('./arquiva.txt', 'w') as arquivo:
-                json.dump(dados, arquivo, indent=4)
+            dados["senha"].append(senha)
+
+            with open('./dados.txt', 'w') as arquivo:
+                json.dump(dados, arquivo, indent = 4)
             break
         except TelefoneError:
             print("O telefone informado não é válida. \nExemplo: 11912345678 \n")
         except CpfError:
             print("O cpf informado não é valido. \nExemplo: 12345678910 \n")
 
-def dados(opcao):
-    match opcao:
-        case 1:
-            cadastro()
-        case 2:
-            login()
+def verifica():
+    verificado = False
+    with open('./dados.txt', 'r') as arquivo:
+        dados = json.load(arquivo)
 
+    cpf, senha = login()
+
+    cpfs = dados.get("cpf", [])
+    senhas = dados.get("senha", [])
+    
+    for i in range(len(cpfs)):
+        if cpf == cpfs[i] and senha == senhas[i]:
+            return True
+    return False
 
 def menu():
     """
     Função principal que controla o menu principal e as ações do usuário.
     """
-    
-    print('''1- Cadastro
+    while True:   
+        print('''1- Cadastro
 2- Login''')
-    opcao = int(input("Escolha uma opção: "))
-    dados(opcao)
+        opcao = int(input("Escolha uma opção: "))
+        
+        if opcao == 1:
+            cadastro()
+        elif opcao == 2:
+            if verifica():
+                print("FOI")
+                # menusec()
+            else:
+                print("Login invalido")
 
 
 menu()
